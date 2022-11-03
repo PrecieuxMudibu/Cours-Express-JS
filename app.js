57bbb477b21f560b52e9970ad416eee547a0f49a
PrecieuxMudibu <oceangpmb@gmail.com>
2022-11-03 16:30:38 +0100

    Find pockemon

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express(); // On crée une instance d'express. On crée notre petit serveur web sur lequel va tourner notre application
 const port = 3000;
+let pockemons = require('./mock-pockemon');
 
-app.get('/', (request, response) => response.send('Hello, Express'));
+app.get('/api/pockemon/:id', (request, response) => {
+    const id= parseInt(request.params.id)
+    const pockemon = pockemons.find(pockemon => id === pockemon.id)
+    response.send(`Le pockemon que vous cherchiez est : ${pockemon.name}`);
+});
 
 app.listen(port, console.log(`Notre application tourne sur le port ${port}`));
